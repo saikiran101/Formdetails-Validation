5990d612baa6a9837da1b9f1513df4f2679a93a6
Namsani saikiran <sai24608@gmail.com>
2022-06-28 18:05:42 +0530

    yarn-lock file missed

---
+++ src/components/Navbar1.js
@@ -0,0 +1,18 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+function Navbar1() {
+	return (
+		<>
+			<Navbar bg="dark" variant="dark">
+				<Container>
+					<Navbar.Brand>
+						PART 1
+					</Navbar.Brand>
+				</Container>
+			</Navbar>
+		</>
+	);
+}
+
+export default Navbar1;
